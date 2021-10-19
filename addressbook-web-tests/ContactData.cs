0dffad9cc5908735f373b26011b849ea86b21db8
Kseniya Grebenyuk <kgrebenyuk@caeonline.com>
2021-10-19 05:17:46 -0500

    Создан тест для контактов.

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        public ContactData(string firstname, string lastname)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Nickname { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string Home { get; set; }
+        public string Mobile { get; set; }
+        public string Work { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string Homepage { get; set; }
+        public string Bday { get; set; }
+        public string Bmonth { get; set; }
+        public string Byear { get; set; }
+        public string Aday { get; set; }
+        public string Amonth { get; set; }
+        public string Ayear { get; set; }
+        public string Address2 { get; set; }
+        public string Phone2 { get; set; }
+        public string Notes { get; set; }
+    }
+}
+
