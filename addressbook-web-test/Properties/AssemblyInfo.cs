c9a21e9f4a88529151f44315256be9857420f236
Kseniya Grebenyuk <kgrebenyuk@caeonline.com>
2021-11-02 04:16:24 -0500

    New Project

---
+++ addressbook-web-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook-web-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook-web-test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7816918e-2ad3-4764-b997-90cc75665ae5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook-web-test/tests/GroupModificationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupModificationTests : AuthTestBase
+    {
+        [Test]
+        public void GroupModificationTest()
+        {
+            GroupData newData = new GroupData("test name upd");
+            newData.Header = null;
+            newData.Footer = null;
+
+            app.Groups.Modify(1, newData);
+        }
+    }
+}
